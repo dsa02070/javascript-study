d59d1477bf5c44a8c36189d9fa08b9e5e6aff8e3
gitname <82920762+dsa02070@users.noreply.github.com>
2021-07-14 23:56:02 +0900

    localStorage.setItem
    localStorage를 이용한 간단 로그인

--- js/main.js
+++ js/main.js
@@ -3,19 +3,33 @@ const loginInput = loginForm.querySelector('input');
 const greeting = document.querySelector('#greeting')
 
 const hidden = 'hidden'
+const UserName_key = "username"
 // const loginButton = loginForm.querySelector('button');
 
 function btnsubmit(e) {
     e.preventDefault(); /* 브라우저가 기본적으로 작동하는 것을 막아준다. */
-    const value = loginInput.value;
-    console.log(e)
-    const username = loginInput.value;
     loginForm.classList.add('hidden');
-    console.log(username)
+    const username = loginInput.value;
+    localStorage.setItem(UserName_key, username)
     // greeting.innerText = 'hello ' + username;
-    greeting.innerText = `hello ${username}`;
-
-    greeting.classList.remove(hidden)
+    paintGreetings(username)
 }
 
-loginForm.addEventListener('submit', btnsubmit)
+let paintGreetings = (username) => {
+    greeting.classList.remove(hidden)
+    greeting.innerText = `hello ${username}`
+}
+
+const savedUserName = localStorage.getItem(UserName_key)
+console.log(savedUserName)
+
+if (savedUserName == null) {
+    loginForm.classList.remove(hidden)
+    loginForm.addEventListener('submit', btnsubmit)
+} else {
+    paintGreetings(savedUserName)
+}
+
+// localStorage.setItem('username', 'nico');
+// localStorage.getItem('username');
+// localStorage.removeItem('username')
